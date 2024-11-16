c5e0b198827d72384493eee7bf452fe1b913ae1d
Leo <57850665+Jay-Chou118@users.noreply.github.com>
2024-11-16 11:03:59 +0800

    Update main.py

--- 粒子群算法/main.py
+++ 粒子群算法/main.py
@@ -1,1 +1,32 @@
-
+def fitness(self, x):
+    # 请根据具体问题定义适应度函数
+    pass
+ 
+def update_velocities(self):
+    r1 = np.random.rand(self.num_dimensions)
+    r2 = np.random.rand(self.num_dimensions)
+    for i in range(self.num_particles):
+        self.velocities[i] = self.w * self.velocities[i] + self.c1 * r1 * (self.pbests[i] - self.particles[i]) + self.c2 * r2 * (self.gbest - self.particles[i])
+ 
+def update_positions(self):
+    for i in range(self.num_particles):
+        self.particles[i] += self.velocities[i]
+ 
+def update_pbests(self):
+    for i in range(self.num_particles):
+        if self.fitness(self.particles[i]) < self.fitness(self.pbests[i]):
+            self.pbests[i] = self.particles[i]
+ 
+def update_gbest(self):
+    for i in range(self.num_particles):
+        if self.fitness(self.particles[i]) < self.fitness(self.gbest):
+            self.gbest = self.particles[i]
+ 
+def run(self):
+    for t in range(self.max_iterations):
+        self.update_velocities()
+        self.update_positions()
+        self.update_pbests()
+        self.update_gbest()
+ 
+    return self.gbest
